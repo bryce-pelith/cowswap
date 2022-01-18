77bd7cba24d1a7e03f902c3f2709ba0f42d9a2b6
Bryce <bryce@pelith.com>
2022-01-19 00:34:12 +0800

    get wallet acount & token balances via built-in hooks

--- src/custom/pages/AssetsDashboard/index.tsx
+++ src/custom/pages/AssetsDashboard/index.tsx
@@ -15,9 +15,19 @@ import { useAllTokens } from '@src/hooks/Tokens'
 const HAKKA = '0x0E29e5AbbB5FD88e28b2d355774e73BD47dE3bcd'
 
 const App = () => {
-  const [connected, setConnected] = useState(false)
-  const [walletAddress, setWalletAddress] = useState('')
-  const [balance, setBalance] = useState<{ [key: string]: number }>({})
+  const { account, chainId } = useActiveWeb3React()
+
+  const ether = useMemo(() => chainId && ExtendedEther.onChain(chainId), [chainId])
+  const allTokens = useAllTokens()
+  const tokens = useMemo(() => Object.values(allTokens), [])
+  const currencies = useMemo(() => {
+    return [ether as Currency, ...tokens]
+  }, [ether, tokens])
+
+  const balances = useCurrencyBalances(account, currencies)
+  const tokenBalances = useTokenBalances(account, tokens)
+
+  const [prices, setPrices] = useState<{ [key: string]: number }>({})
 
   // pie chart
   // const [activeIndex, setActiveIndex] = useState(0)
@@ -28,94 +38,23 @@ const App = () => {
   //   [setActiveIndex]
   // )
 
-  const rate = useMemo(() => {
-    // fetch token price
-    const rateMap: { [key: string]: number } = { HAKKA: 1 }
-    fetch(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${TokenList.map(({ id }) => {
-        return id
-      }).join(',')}&vs_currencies=twd`
-    )
-      .then((response) => response.json())
-      .then((price) => {
-        TokenList.map(({ id, symbol }) => (rateMap[symbol] = price[id].twd / price['hakka-finance'].twd))
-      })
-    return rateMap
-  }, [])
-
-  useEffect(() => {
-    if (!walletAddress) return
-    const newBalance: { [key: string]: number } = { ETH: 0 }
-    web3.eth.getBalance(walletAddress).then((balance) => {
-      newBalance.ETH = parseFloat(balance) / 10 ** 18
-    })
-    // fetch token balance
-    Promise.all(
-      TokenList.filter(({ symbol }) => symbol !== 'ETH').map(({ symbol, address, decimals }) => {
-        return new web3.eth.Contract(ERC20_ABI as AbiItem[], address).methods
-          .balanceOf(walletAddress)
-          .call()
-          .then((amount: number) => {
-            newBalance[symbol] = amount / 10 ** (decimals || 0)
-          })
-      })
-    ).then(() => {
-      setBalance(newBalance)
-    })
-  }, [walletAddress])
-
-  const totalValue = useMemo(() => {
-    return TokenList.filter(({ symbol }) => !!balance[symbol] && !!rate[symbol])
-      .map(({ symbol }) => balance[symbol] * rate[symbol])
-      .reduce((a, b) => a + b, 0)
-  }, [balance, rate])
-
-  const chartValue = useMemo(() => {
-    return TokenList.filter(({ symbol }) => !!balance[symbol] && !!rate[symbol])
-      .map(({ name, symbol }) => {
-        return { name, symbol, value: balance[symbol] * rate[symbol] }
-      })
-      .sort((t0, t1) => t1.value - t0.value)
-  }, [balance, rate])
-
-  const connectMetaMask = useCallback(() => {
-    web3.givenProvider.request({ method: 'eth_requestAccounts' }).then(() => {
-      setConnected(true)
-      web3.eth
-        .getAccounts()
-        .then((accounts) => setWalletAddress('0xc1f9bb72216e5ecdc97e248f65e14df1fe46600a' || accounts[0]))
-    })
-  }, [])
-
   return (
     <>
-      <button className="w-48 text-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-        <div
-          role="button"
-          onClick={() => {
-            connectMetaMask()
-          }}
-          onKeyDown={() => {
-            connectMetaMask()
-          }}
-          tabIndex={0}
-        >
-          {connected ? `${walletAddress.substring(0, 10)}...${walletAddress.substring(36, 42)}` : 'connect MetaMask'}
-        </div>
-      </button>
-      <div>
-        {connected &&
-          TokenList.filter(({ symbol }) => !!balance[symbol])
-            .sort((t0, t1) => balance[t1.symbol] * rate[t1.symbol] - balance[t0.symbol] * rate[t0.symbol])
-            .map((token) => (
-              <AltToken
-                key={token.id}
-                token={token}
-                balance={balance[token.symbol]}
-                value={balance[token.symbol] * rate[token.symbol]}
-              />
-            ))}
-      </div>
+      {balances
+        .filter((amount) => !isEmpty(amount))
+        .map((amount: any) => {
+          const address: string = amount.currency.address ?? 'ETH'
+          return (
+            <CurrencyRow
+              key={address}
+              id={address}
+              currency={amount.currency}
+              balance={formatCurrencyAmount(amount, 4, address === HAKKA ? 0 : undefined)}
+              // value={''}
+              value={((parseFloat(amount.toExact()) * prices[address]) / prices.HAKKA).toFixed(0)}
+            />
+          )
+        })}
       {/* <div className="py-4">Total Assets: {totalValue.toFixed(2)} HAKKA</div> */}
       {/* <PieChart width={1000} height={400}>
         <Pie
@@ -135,21 +74,45 @@ const App = () => {
   )
 }
 
-type AltTokenProps = { token: { [key: string]: string | number | undefined }; balance: number; value: number }
+const CurrencyRow = ({
+  id,
+  currency,
+  balance,
+  value,
+}: {
+  id: string
+  currency: Currency
+  balance: string
+  value: string
+}) => {
+  // const {
+  //   v2Trade,
+  //   // v3TradeState: { trade: v3Trade, state: v3TradeState },
+  //   // toggledTrade: trade,
+  //   allowedSlippage,
+  //   currencyBalances,
+  //   parsedAmount,
+  //   currencies,
+  //   inputError: swapInputError,
+  // } = useDerivedSwapInfo(/* toggledVersion */)
 
-const AltToken = ({ token, balance, value }: AltTokenProps) => {
-  const { name, symbol, logoURI } = token
-  return balance > 0 ? (
-    <div className="flex flex-row p-1">
-      <div>
-        <img src={logoURI as string} alt={symbol as string} className="w-6 mx-1"></img>
+  // logTradeDetails(v2Trade, allowedSlippage)
+
+  return (
+    <>
+      <div className="flex flex-row p-1">
+        <CurrencyLogo style={{ marginRight: '0.5rem' }} currency={currency} size={'24px'} />
+        <div className="w-64">{currency.name}</div>
+        <div className="w-24">{balance}</div>
+        <div className="w-16 text-right">{currency.symbol}</div>
+        <div className="w-16 text-right">~=</div>
+        <div className="w-64 text-right">{value} HAKKA</div>
+        <div className="w-16 text-right">
+          <a href={`#/swap/${id}`}>Sell</a>
+        </div>
       </div>
-      <div className="w-64">{name}</div>
-      <div className="w-64">{balance}</div>
-      <div>~=</div>
-      <div className="w-64 text-right">{value.toFixed(2)} HAKKA</div>
-    </div>
-  ) : null
+    </>
+  )
 }
 
 type activeShapeProps = {
